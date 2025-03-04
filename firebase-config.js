0263538138a291eef79780229f59d4f179b8230d
PortaldeloteriasMacPoint <portaldeloteriasmacpoint@gmail.com>
2025-03-04 12:34:55 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase (já com suas credenciais)
+const firebaseConfig = {
+    apiKey: "AIzaSyAO3As6jMMmENtzaK9zlDADbpS9UlNxx8o",
+    authDomain: "mac-projeto-4e552.firebaseapp.com",
+    projectId: "mac-projeto-4e552",
+    storageBucket: "mac-projeto-4e552.appspot.com",
+    messagingSenderId: "537330451519",
+    appId: "1:537330451519:web:5a1b4c921119b5ee98e48a"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
